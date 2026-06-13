a6b04b4741b805ccc697256ef1e519591adcb857
Nirmal Chhetri <chhetrinirmal765@gmail.com>
2026-06-13 16:27:35 +0545

    refactor: refactored notification service with better names and structure

--- lib/features/network/presentation/cubits/chat_cubit.dart
+++ lib/features/network/presentation/cubits/chat_cubit.dart
@@ -42,7 +42,7 @@ class ChatCubit extends Cubit<ChatState> {
     try {
       userId = int.tryParse(await sessionService.userId);
       final token = await sessionService.token;
-      final uri = Uri.parse('ws://10.197.8.138:4000/v1/chat/$id');
+      final uri = Uri.parse('ws://10.30.198.138:4000/v1/chat/$id');
       _channel = IOWebSocketChannel.connect(
         uri,
         headers: {'Authorization': 'Bearer $token'},
